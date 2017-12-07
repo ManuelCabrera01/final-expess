c277d3509a779331bbf0e12441a616ee6b45495b
Manuel <m.a.c.p90@gmail.com>
2017-12-07 12:18:35 -0500

    rides have all the route... waiting for bugs :\

--- routes/rides-routes.js
+++ routes/rides-routes.js
@@ -7,7 +7,7 @@ const upload = require('../configs/multer');
 const router = express.Router();
 
 
-
+/* GET rides listing. */
 // router.get('/showRides', (req, res, next) => {
 //   Phone.find((err, rideList) => {
 //     if (err) {
@@ -18,7 +18,7 @@ const router = express.Router();
 //   });
 // });
 
-
+/*post new ride*/
 router.post('/rides', upload.single('file'), function(req, res) {
   const phone = new Phone({
     name: req.body.name,
@@ -40,3 +40,65 @@ router.post('/rides', upload.single('file'), function(req, res) {
     });
   });
 });
+
+/* GET a single ride. */
+router.get('/ride/:id', (req, res) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Rides.findById(req.params.id, (err, theRide) => {
+      if (err) {
+        res.json(err);
+        return;
+      }
+
+      res.json(theRide);
+    });
+});
+
+/* EDIT a Phone. */
+router.put('/ridess/:id', (req, res) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  const updates = {
+    name: req.body.name,
+    date: req.body.date,
+    category: req.body.category,
+    distnace: req.body.distnace,
+    // map: `/uploads/${req.file.filename}`,
+  };
+
+  Rides.findByIdAndUpdate(req.params.id, updates, (err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+
+    res.json({
+      message: 'Ride change have been save'
+    });
+  });
+})
+router.delete('/rides/:id', (req, res) => {
+  if(!mongoose.Types.ObjectId.isValid(req.params.id)) {
+    res.status(400).json({ message: 'Specified id is not valid' });
+    return;
+  }
+
+  Rides.remove({ _id: req.params.id }, (err) => {
+    if (err) {
+      res.json(err);
+      return;
+    }
+
+    return res.json({
+      message: 'Ride has been deleted!'
+    });
+  })
+});
+module.exports = router;
